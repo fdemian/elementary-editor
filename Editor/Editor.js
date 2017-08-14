b541d699ae3ae537d4af124c23b71d3e95a306b4
fdemian <demian.fc@gmail.com>
2017-08-13 21:41:26 -0300

    Return the entire editor state not just the "currentContent".

--- Editor/Editor.js
+++ Editor/Editor.js
@@ -117,7 +117,7 @@ class EditorComponent extends React.Component {
    this.toggleInlineStyle = (style) => this._toggleInlineStyle(style);
    this.spoilerBlockRender = (style) => this._spoilerBlockRender(style);
    this.selectionIsCollapsed = () => this._selectionIsCollapsed();
-   this.getContent = () => this.state.editorState.toJS().currentContent; //console.log(state); //
+   this.getContent = () => this.state.editorState.toJS(); //console.log(state); //
    this.blockIsActive = (block) => this._blockIsActive(block);
    this.inlineIsActive = (style) => this._inlineIsActive(style);
    this.customBlockIsActive = (block) => this._customBlockIsActive(block);
