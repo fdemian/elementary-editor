e9d1f972b6ce174c9308c957fc3880eb482f2014
fdemian <demian.fc@gmail.com>
2018-01-20 16:03:15 -0300

    Add rendering logic for a LaTeX block.

---
+++ Editor/TextElements/Latex/LatexBlock.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import katex from 'katex';
+
+class LatexBlock extends Component {
+	
+  constructor(props) {
+    super(props);
+    this._timer = null;
+  }
+
+  _update() {
+    
+	if (this._timer) {
+	  alert("clearTimeout!");
+      clearTimeout(this._timer);
+    }
+
+    this._timer = setTimeout(() => {
+      katex.render(
+        this.props.content,
+        this.refs.container,
+        {displayMode: true}
+      );
+    }, 0);
+	
+  }
+  
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.content !== this.props.content) {
+      this._update();
+    }
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this._timer);
+    this._timer = null;
+  }
+
+  componentDidMount() {
+    this._update();
+  }
+		
+  render() {		
+	return <span ref="container"></span>;
+  }	  
+}
+
+export default LatexBlock;
