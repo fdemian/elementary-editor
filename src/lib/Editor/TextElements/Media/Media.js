167b3719c2ae5e33d424022aaab9fb00c06a1506
fdemian <demian.fc@gmail.com>
2019-07-12 21:11:07 -0300

    Version 0.8.14. pfew.

--- src/lib/Editor/TextElements/Media/Media.js
+++ src/lib/Editor/TextElements/Media/Media.js
@@ -2,18 +2,18 @@ import React from 'react';
 import ReactPlayer from 'react-player';
 
 const Media = (props) => {
-  
+
   let src;
   const { contentState, block } = props;
 
-  if(contentState){
+  if(contentState !== null && contentState !== undefined){
     const entity = contentState.getEntity(block.getEntityAt(0))
     src = entity.getData().src;
   }
   else {
     src = props.src;
   }
-
+  
   return(<ReactPlayer url={src} playing={false} />);
 }
 
